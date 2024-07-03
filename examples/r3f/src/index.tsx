a91c7189acc9d389e9754ada189b5dc7b6606a32
Andrea Louis <76847598+alouis-jpg@users.noreply.github.com>
2024-07-03 18:02:53 +0200

    testing multitilesets in same scene: resetTransform places each tileset independantly resulting in one of them being upside down

--- examples/r3f/src/index.tsx
+++ examples/r3f/src/index.tsx
@@ -1,37 +1,65 @@
-import * as ReactDOM from 'react-dom/client'
-import { useState, useRef, Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { PerspectiveCamera, OrbitControls } from '@react-three/drei'
+import * as ReactDOM from 'react-dom/client';
+import { useState, useRef, Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { PerspectiveCamera, OrbitControls } from '@react-three/drei';
 import { ErrorBoundary } from 'react-error-boundary';
-import { Matrix4, Euler } from 'three'
+import { Matrix4, Euler } from 'three';
 
-import { Loader3DTilesR3FAsset } from './loader-3dtiles-r3f'
+import { Loader3DTilesR3FAsset } from './loader-3dtiles-r3f';
 
 function App() {
   const camera = useRef(null);
 
   return (
     <div id="canvas-container">
-      <Canvas style={{ background: '#272730'}}>
+      <Canvas style={{ background: '#272730' }}>
+        {/* <Canvas style={{ background: '#272730' }}  camera={{ fov: 45, position: [0, 0, 0] }}> */}
+        {/* <Canvas style={{ background: '#272730' }}  camera={{ fov: 45, position: [4103371.51391358, 4707583.85766857, -1237856.306154429] }}> */}
+        {/* <mesh>
+          <boxGeometry />
+          <meshBasicMaterial color="red" />
+        </mesh> */}
+        {/* <mesh position={[4103371.51391358, 4707583.85766857, -1237856.306154429]}>
+          <boxGeometry />
+          <meshBasicMaterial color="blue" />
+        </mesh> */}
         <PerspectiveCamera ref={camera}>
-          <ErrorBoundary fallbackRender={() => (
-            <mesh>
-              <sphereGeometry />
-              <meshBasicMaterial color="red" />
-            </mesh>
-          )}>
-            <Suspense fallback={
+          {/* <PerspectiveCamera ref={camera} position={[1334910.452860931, 4138104, -4653115.0285432]}></PerspectiveCamera> */}
+          {/* <OrbitControls  target={[4103371.51391358, 4707583.85766857, -1237856.306154429]} minDistance={20} maxDistance={30} /> */}
+          {/* <OrbitControls target={[0, 0, 0]} minDistance={0.1} maxDistance={3000} /> */}
+          <ErrorBoundary
+            fallbackRender={() => (
               <mesh>
-                <sphereGeometry />
-                <meshBasicMaterial color="yellow" />
+                <boxGeometry />
+                <meshBasicMaterial color="pink" />
               </mesh>
-            }>
+            )}
+          >
+            <Suspense
+              fallback={
+                <mesh>
+                  <sphereGeometry />
+                  <meshBasicMaterial color="yellow" />
+                </mesh>
+              }
+            >
               <Loader3DTilesR3FAsset
-                 dracoDecoderPath={"https://unpkg.com/three@0.160.0/examples/jsm/libs/draco"}
-                 basisTranscoderPath={"https://unpkg.com/three@0.160.0/examples/jsm/libs/basis"}
-                 rotation={new Euler(-Math.PI / 2, 0, 0)}
-                 url="https://int.nyt.com/data/3dscenes/ONA360/TILESET/0731_FREEMAN_ALLEY_10M_A_36x8K__10K-PN_50P_DB/tileset_tileset.json"
+                dracoDecoderPath={'https://unpkg.com/three@0.160.0/examples/jsm/libs/draco'}
+                basisTranscoderPath={'https://unpkg.com/three@0.160.0/examples/jsm/libs/basis'}
+                rotation={new Euler(-Math.PI / 2, 0, 0)}
+                url="http://192.168.10.123:8080/Interieur/Coupoles/3DTiles/merge_int_ext_S2_S3/tileset.json"
+                // url="https://int.nyt.com/data/3dscenes/ONA360/TILESET/0731_FREEMAN_ALLEY_10M_A_36x8K__10K-PN_50P_DB/tileset_tileset.json"
+                // url='/tileset_Vatican_basilique_interieur_cupola_N3_5M_4x16k.json'
+                // url='https://tile.googleapis.com/v1/3dtiles/root.json?key=AIzaSyD2im4uSnZQefEFJIlfSpSD39apMdiiKFE'
+                // url='https://tile.googleapis.com/v1/3dtiles/root.json?'
+                // googleApiKey='AIzaSyD2im4uSnZQefEFJIlfSpSD39apMdiiKFE'
                 maximumScreenSpaceError={48}
+                resetTransform={true}
+                // orientToGeocoord={{
+                //   height: 0,
+                //   lat: 40.721513285585736,
+                //   long: -73.99266363643088,
+                // }}
               />
             </Suspense>
           </ErrorBoundary>
@@ -39,7 +67,7 @@ function App() {
         <OrbitControls camera={camera.current} />
       </Canvas>
     </div>
-  )
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- examples/r3f/src/loader-3dtiles-r3f.tsx
+++ examples/r3f/src/loader-3dtiles-r3f.tsx
@@ -1,6 +1,6 @@
-import { Loader3DTiles, LoaderProps, Runtime } from 'three-loader-3dtiles'
-import { useLoader, useThree, useFrame } from '@react-three/fiber'
-import { Loader, Vector2 } from 'three'
+import { Loader3DTiles, LoaderProps, Runtime } from 'three-loader-3dtiles';
+import { useLoader, useThree, useFrame } from '@react-three/fiber';
+import { Loader, Vector2 } from 'three';
 
 class Loader3DTilesBridge extends Loader {
   props: LoaderProps;
@@ -9,23 +9,32 @@ class Loader3DTilesBridge extends Loader {
     const loadTileset = async () => {
       try {
         const result = await Loader3DTiles.load({
-           url,
-           ...this.props,
-          onProgress
-         })
-         onLoad(result);
-      }
-      catch(e) {
-        console.log("Error loading 3d tiles!", e);
+          url,
+          ...this.props,
+          onProgress,
+        });
+        onLoad(result);
+        // result.runtime.orientToGeocoord({
+        //   height: 10,
+        //   lat: 40.721513285585736,
+        //   long: -73.99266363643088,
+        // });
+        // console.log(result);
+        // result.runtime.setDebug(true)
+        // result.runtime.showTiles(true);
+        // result.runtime.setRenderer(this.props.renderer)
+        // is not a function although is implemented in Loader3DTiles class (see https://github.com/nytimes/three-loader-3dtiles/blob/48cb4b30ce3558ad04d393bda9a1d78a50294569/src/index.ts#L616C9-L616C20)
+        // result.runtime.setViewport(this.props.viewport);
+      } catch (e) {
         onError(e);
       }
-    }
+    };
     loadTileset();
   }
   setProps(props) {
     this.props = props;
   }
-};
+}
 
 function Loader3DTilesR3FAsset(props) {
   const threeState = useThree();
@@ -33,33 +42,33 @@ function Loader3DTilesR3FAsset(props) {
     renderer: threeState.gl,
     viewport: getViewport(threeState.gl),
     options: {
-      ...props
-    }
-  }
+      ...props,
+    },
+  };
 
   // TODO: Getting type error
   // @ts-ignore
-  const { model, runtime } = useLoader(Loader3DTilesBridge, props.url, (loader:Loader3DTilesBridge) => {
-    loader.setProps(loaderProps);    
-  })
+  const { model, runtime } = useLoader(Loader3DTilesBridge, props.url, (loader: Loader3DTilesBridge) => {
+    loader.setProps(loaderProps);
+  });
 
   useFrame(({ size, camera }, dt) => {
-    runtime.update(dt, size.height, camera);
+    runtime.update(dt, camera);
   });
 
   return (
     <group {...props} dispose={runtime.dispose}>
       <primitive object={model} />
     </group>
-  )
+  );
 }
 function getViewport(renderer) {
   const viewSize = renderer.getSize(new Vector2());
   return {
     width: viewSize.x,
     height: viewSize.y,
-    devicePixelRatio: renderer.getPixelRatio()
-  }
+    devicePixelRatio: renderer.getPixelRatio(),
+  };
 }
 
-export { Loader3DTilesR3FAsset }
+export { Loader3DTilesR3FAsset };
